4c7e0e07bc9d74d97c64900794cdc1b30592c5d2
Mbah-Javis <jelvismbah18@gmail.com>
2022-05-20 11:55:56 +0100

    Update validate_transaction.dart

--- lib/screens/validate_transaction/validate_transaction.dart
+++ lib/screens/validate_transaction/validate_transaction.dart
@@ -80,225 +80,235 @@ class _ValidateTransaction extends State<ValidateTransaction>{
               ],
             ),
           ),
-          const SizedBox(height: 50,),
-          Container(
-            alignment: Alignment.topLeft,
-            margin: const EdgeInsets.only(left: 20, top: 10, bottom: 5),
-            child: Text('Your transaction',
-              style: TextStyle(
-                color: kPrimaryColor,
-                fontSize: 22,
-                fontWeight: FontWeight.bold
-              ),
-            ),
-          ),
-          Container(
-            margin: const EdgeInsets.only(left: 10, bottom: 10, top: 15, right: 10),
-            padding: EdgeInsets.all(15),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-                border: Border.all(width: 1, color: kBorderColor)
-            ),
-            child: Column(
-              children: [
-                Container(
-                  padding: EdgeInsets.only(bottom: 15, top: 10),
-                  decoration: BoxDecoration(
-                      border: Border(
-                          bottom: BorderSide(width: 1.0, color: kBorderColor)
-                      )
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const Text('Sending ',
+          Expanded(
+              child: SingleChildScrollView(
+                physics: const BouncingScrollPhysics(),
+                child: Column(
+                  children: [
+                    const SizedBox(height: 20,),
+                    Container(
+                      alignment: Alignment.topLeft,
+                      margin: const EdgeInsets.only(left: 20, top: 10, bottom: 5),
+                      child: Text('Your transaction',
                         style: TextStyle(
-                            color: kGreyColor,
-                            fontSize: 18,
-                            fontWeight: FontWeight.w700
-                        ),
-                      ),
-                      Text(widget.amount.toString()+ ' XAF',
-                        style: const TextStyle(
-                            color: Colors.black,
+                            color: kPrimaryColor,
                             fontSize: 22,
                             fontWeight: FontWeight.bold
                         ),
                       ),
-                    ],
-                  ),
-                ),
-                const SizedBox(height: 20,),
-                Container(
-                  padding: EdgeInsets.only(bottom: 15, top: 5),
-                  decoration: BoxDecoration(
-                      border: Border(
-                          bottom: BorderSide(width: 1.0, color: kBorderColor)
-                      )
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const Text('To ',
-                        style: TextStyle(
-                            color: kGreyColor,
-                            fontSize: 18,
-                            fontWeight: FontWeight.w700
-                        ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(left: 10, bottom: 10, top: 15, right: 10),
+                      padding: EdgeInsets.all(15),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          border: Border.all(width: 1, color: kBorderColor)
                       ),
-                      Text(widget.phoneNumber.toString(),
-                        style: const TextStyle(
-                            color: Colors.black,
-                            fontSize: 22,
-                            fontWeight: FontWeight.bold
-                        ),
+                      child: Column(
+                        children: [
+                          Container(
+                            padding: EdgeInsets.only(bottom: 15, top: 10),
+                            decoration: BoxDecoration(
+                                border: Border(
+                                    bottom: BorderSide(width: 1.0, color: kBorderColor)
+                                )
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                const Text('Sending ',
+                                  style: TextStyle(
+                                      color: kGreyColor,
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.w700
+                                  ),
+                                ),
+                                Text(widget.amount.toString()+ ' XAF',
+                                  style: const TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 22,
+                                      fontWeight: FontWeight.bold
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          const SizedBox(height: 20,),
+                          Container(
+                            padding: EdgeInsets.only(bottom: 15, top: 5),
+                            decoration: BoxDecoration(
+                                border: Border(
+                                    bottom: BorderSide(width: 1.0, color: kBorderColor)
+                                )
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                const Text('To ',
+                                  style: TextStyle(
+                                      color: kGreyColor,
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.w700
+                                  ),
+                                ),
+                                Text(widget.phoneNumber.toString(),
+                                  style: const TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 22,
+                                      fontWeight: FontWeight.bold
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          const SizedBox(height: 20,),
+                          Container(
+                            padding: EdgeInsets.only(bottom: 15, top: 5),
+                            decoration: BoxDecoration(
+                                border: Border(
+                                    bottom: BorderSide(width: 1.0, color: kBorderColor)
+                                )
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                const Text('Mobile money provider ',
+                                  style: TextStyle(
+                                      color: kGreyColor,
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.w700
+                                  ),
+                                ),
+                                Text('MTN',
+                                  style: const TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 22,
+                                      fontWeight: FontWeight.bold
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          const SizedBox(height: 20,),
+                          Container(
+                            padding: EdgeInsets.only(bottom: 15, top: 5),
+                            decoration: BoxDecoration(
+                                border: Border(
+                                    bottom: BorderSide(width: 1.0, color: kBorderColor)
+                                )
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                const Text('Service fee ',
+                                  style: TextStyle(
+                                      color: kGreyColor,
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.w700
+                                  ),
+                                ),
+                                Text('250.0 XAF',
+                                  style: const TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 22,
+                                      fontWeight: FontWeight.bold
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          const SizedBox(height: 20,),
+                          Container(
+                            padding: EdgeInsets.only(bottom: 15, top: 5),
+                            decoration: BoxDecoration(
+                                border: Border(
+                                    bottom: BorderSide(width: 1.0, color: kBorderColor)
+                                )
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                const Text('Tax ',
+                                  style: TextStyle(
+                                      color: kGreyColor,
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.w700
+                                  ),
+                                ),
+                                Text('0 XAF',
+                                  style: const TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 22,
+                                      fontWeight: FontWeight.bold
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          const SizedBox(height: 20,),
+                          Container(
+                            padding: EdgeInsets.only(bottom: 15, top: 5),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                const Text('Total amount ',
+                                  style: TextStyle(
+                                      color: kGreyColor,
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.w700
+                                  ),
+                                ),
+                                Text('50250.0 XAF',
+                                  style: const TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 22,
+                                      fontWeight: FontWeight.bold
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
-                ),
-                const SizedBox(height: 20,),
-                Container(
-                  padding: EdgeInsets.only(bottom: 15, top: 5),
-                  decoration: BoxDecoration(
-                      border: Border(
-                          bottom: BorderSide(width: 1.0, color: kBorderColor)
-                      )
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const Text('Mobile money provider ',
-                        style: TextStyle(
-                            color: kGreyColor,
-                            fontSize: 18,
-                            fontWeight: FontWeight.w700
+                    ),
+                    const SizedBox(height: 20,),
+                    SizedBox(
+                      width: 200,
+                      height: 43,
+                      child: TextButton(
+                        onPressed: () {
+                        },
+                        style:TextButton.styleFrom(
+                          primary: kPrimaryAccentColor,
+                          backgroundColor: kPrimaryAccentColor,
+                          shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(30))),
+                          elevation: 3,
                         ),
-                      ),
-                      Text('MTN',
-                        style: const TextStyle(
-                            color: Colors.black,
-                            fontSize: 22,
-                            fontWeight: FontWeight.bold
+                        child:Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: const [
+                            SizedBox(width: 10,),
+                            Icon(
+                              Icons.check_circle_rounded,
+                              size: 25,
+                              color: Colors.white,
+                            ),
+                            SizedBox(width: 10,),
+                            Text('Confirm Payment', style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                            ),),
+                            SizedBox(width: 10,),
+                          ],
                         ),
-                      ),
-                    ],
-                  ),
-                ),
-                const SizedBox(height: 20,),
-                Container(
-                  padding: EdgeInsets.only(bottom: 15, top: 5),
-                  decoration: BoxDecoration(
-                      border: Border(
-                          bottom: BorderSide(width: 1.0, color: kBorderColor)
-                      )
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const Text('Service fee ',
-                        style: TextStyle(
-                            color: kGreyColor,
-                            fontSize: 18,
-                            fontWeight: FontWeight.w700
-                        ),
-                      ),
-                      Text('250.0 XAF',
-                        style: const TextStyle(
-                            color: Colors.black,
-                            fontSize: 22,
-                            fontWeight: FontWeight.bold
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                const SizedBox(height: 20,),
-                Container(
-                  padding: EdgeInsets.only(bottom: 15, top: 5),
-                  decoration: BoxDecoration(
-                      border: Border(
-                          bottom: BorderSide(width: 1.0, color: kBorderColor)
-                      )
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const Text('Tax ',
-                        style: TextStyle(
-                            color: kGreyColor,
-                            fontSize: 18,
-                            fontWeight: FontWeight.w700
-                        ),
-                      ),
-                      Text('0 XAF',
-                        style: const TextStyle(
-                            color: Colors.black,
-                            fontSize: 22,
-                            fontWeight: FontWeight.bold
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                const SizedBox(height: 20,),
-                Container(
-                  padding: EdgeInsets.only(bottom: 15, top: 5),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const Text('Total amount ',
-                        style: TextStyle(
-                            color: kGreyColor,
-                            fontSize: 18,
-                            fontWeight: FontWeight.w700
-                        ),
-                      ),
-                      Text('50250.0 XAF',
-                        style: const TextStyle(
-                            color: Colors.black,
-                            fontSize: 22,
-                            fontWeight: FontWeight.bold
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-          SizedBox(height: 20,),
-          SizedBox(
-            width: 200,
-            height: 43,
-            child: TextButton(
-              onPressed: () {
-              },
-              style:TextButton.styleFrom(
-                primary: kPrimaryAccentColor,
-                backgroundColor: kPrimaryAccentColor,
-                shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(30))),
-                elevation: 3,
-              ),
-              child:Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  SizedBox(width: 10,),
-                  Icon(
-                    Icons.check_circle_rounded,
-                    size: 25,
-                    color: Colors.white,
-                  ),
-                  SizedBox(width: 10,),
-                  Text('Confirm Payment', style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 18,
-                  ),),
-                  SizedBox(width: 10,),
-                ],
-              ),
 
-            ),
-          ),
+                      ),
+                    ),
+                    const SizedBox(height: 20,),
+                  ],
+                ),
+              )
+          )
         ],
       ),
     );
