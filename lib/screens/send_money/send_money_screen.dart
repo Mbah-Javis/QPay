3459af1d6a0894a7a58af297aa6f872722485090
Mbah-Javis <jelvismbah18@gmail.com>
2022-05-30 23:41:09 +0100

    Update send_money_screen.dart

---
+++ lib/screens/send_money/send_money_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SendMoneyScreen extends StatefulWidget{
+  const SendMoneyScreen({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _SendMoneyScreen();
+
+}
+
+class _SendMoneyScreen extends State<SendMoneyScreen>{
+  @override
+  Widget build(BuildContext context) {
+   return Scaffold(
+     body: Column(
+       children: [
+
+       ],
+     ),
+   );
+  }
+
+}
