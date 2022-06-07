fe703d036208f88304a76793fd2286fbaf1432a1
Mbah-Javis <jelvismbah18@gmail.com>
2022-06-07 01:38:14 +0100

    Update transaction_widget.dart

---
+++ lib/screens/widgets/transaction_widget.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class TransactionWidget extends StatelessWidget{
+  const TransactionWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+
+}
