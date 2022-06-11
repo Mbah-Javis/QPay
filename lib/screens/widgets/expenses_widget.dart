a10cfd36b7998283c26de235f43293a3aa3588f8
Mbah-Javis <jelvismbah18@gmail.com>
2022-06-11 14:34:46 +0100

    update

--- lib/screens/widgets/expenses_widget.dart
+++ lib/screens/widgets/expenses_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:qpay/constants.dart';
 
 class ExpensesWidget extends StatelessWidget {
@@ -8,49 +9,89 @@ class ExpensesWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: EdgeInsets.only(left: 5),
       margin: const EdgeInsets.all(10),
-      padding: const EdgeInsets.all(15),
-      decoration: BoxDecoration(boxShadow: const [
-        BoxShadow(
-          color: Color(0x4B6D6679),
-          offset: Offset(0.0, 1.0),
-          blurRadius: 6.0,
+      decoration: BoxDecoration(
+        color: kGreenColor,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(20),
+          bottomLeft: Radius.circular(20),
+          topRight: Radius.circular(40),
+          bottomRight: Radius.circular(40),
         )
-      ], color: Colors.white, borderRadius: BorderRadius.circular(20)),
-      child: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Row(
-                children: [
-                  Image.asset(
-                    'assets/images/expenses.png',
-                    height: 55,
-                    width: 55,
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(left: 15),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "Breakfast",
-                          style: TextStyle(color: kGreyColor, fontSize: 17),
-                        ),
-                        SizedBox(
-                          width: 50,
-                        ),
-                        Text(DateTime.now().toString())
-                      ],
+      ),
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
+            boxShadow: const [BoxShadow(
+              color: Color(0x4B6D6679),
+              offset: Offset(0.0, 1.0),
+              blurRadius: 6.0,
+            ),],
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(20)
+        ),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Image.asset(
+                      'assets/images/expenses.png',
+                      height: 55,
+                      width: 55,
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(left: 15),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            width: 120,
+                            child: Text(
+                              "Bag of rice",
+                              style: TextStyle(color: kGreyColor, fontSize: 17),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 6,
+                          ),
+                          Text(
+                            "To: 677730204",
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 6,
+                          ),
+                          Text(
+                            DateFormat.yMMMd().format(DateTime.now()) +
+                                " | " +
+                                DateFormat.jm().format(DateTime.now()),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                Container(
+                  child: Text(
+                    '-20000 XAF',
+                    style: TextStyle(
+                      color: kPrimaryAccentColor,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                ],
-              ),
-              Container(child: Text('500 XAF')),
-            ],
-          )
-        ],
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
