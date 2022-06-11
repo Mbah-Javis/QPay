6604bda680eaf87af525f8d2a1cbec1eb2835e93
Mbah-Javis <jelvismbah18@gmail.com>
2022-06-11 13:43:47 +0100

    Update expenses_widget.dart

--- lib/screens/widgets/expenses_widget.dart
+++ lib/screens/widgets/expenses_widget.dart
@@ -10,48 +10,48 @@ class ExpensesWidget extends StatelessWidget {
     return Container(
       margin: const EdgeInsets.all(10),
       padding: const EdgeInsets.all(15),
-      decoration: BoxDecoration(
-          boxShadow: const [
-            BoxShadow(
-              color: Color(0x4B6D6679),
-              offset: Offset(0.0, 1.0),
-              blurRadius: 6.0,
-            )
-          ],
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(20)),
+      decoration: BoxDecoration(boxShadow: const [
+        BoxShadow(
+          color: Color(0x4B6D6679),
+          offset: Offset(0.0, 1.0),
+          blurRadius: 6.0,
+        )
+      ], color: Colors.white, borderRadius: BorderRadius.circular(20)),
       child: Column(
         children: [
           Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Image.asset('assets/images/expenses.png', height: 55, width: 55,),
-              Container(
-                margin: EdgeInsets.only(left: 15),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+              Row(
+                children: [
+                  Image.asset(
+                    'assets/images/expenses.png',
+                    height: 55,
+                    width: 55,
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(left: 15),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text("Breakfast", style: TextStyle(
-                          color: kGreyColor,
-                          fontSize: 17
-                        ),),
-                        SizedBox(width: 50,),
-                        Container(
-                            child: Text('500 XAF'))
+                        Text(
+                          "Breakfast",
+                          style: TextStyle(color: kGreyColor, fontSize: 17),
+                        ),
+                        SizedBox(
+                          width: 50,
+                        ),
+                        Text(DateTime.now().toString())
                       ],
                     ),
-
-                    Text(DateTime.now().toString())
-                  ],
-                ),
+                  ),
+                ],
               ),
+              Container(child: Text('500 XAF')),
             ],
           )
         ],
       ),
     );
   }
-
 }
